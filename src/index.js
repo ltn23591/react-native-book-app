1621fb3064c694be5e5e9d4c7733bc09cd0ddd50
ltn23591 <ltn23591@gmail.com>
2025-06-01 14:01:32 +0700

    move index.js into src

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv/config');
-const authRoutes = require('./src/routes/authRoutes');
-const bookRoutes = require('./src/routes/bookRoutes');
-const connectDB = require('./src/config/db');
+const authRoutes = require('./routes/authRoutes');
+const bookRoutes = require('./routes/bookRoutes');
+const connectDB = require('./config/db');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
