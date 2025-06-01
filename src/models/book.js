6172cf087c8948bcff052871ae641d7567376a32
ltn23591 <ltn23591@gmail.com>
2025-06-01 21:43:16 +0700

    them truong image

--- src/models/book.js
+++ src/models/book.js
@@ -4,6 +4,7 @@ const bookSchema = new mongoose.Schema(
         title: { type: String, required: true },
         caption: { type: String, required: true },
         rating: { type: Number, min: 1, max: 5 },
+        image: { type: String, required: true },
         user: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User', // Liên kết với mô hình User
